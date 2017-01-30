ac0b0f10a979f122ba6ce418273fa104f5d1b3cf
admincenec <admincenec@PC104.cenecmalaga.net>
2017-01-30 12:31:18 +0100

    Terminado ejercicio34

--- src/Ejercicio34/Ejercicio34.java
+++ src/Ejercicio34/Ejercicio34.java
@@ -1,6 +1,5 @@
 package Ejercicio34;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Ejercicio34 {
@@ -8,7 +7,7 @@ public class Ejercicio34 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a = {1,2,3,4,5};
-		a = BorraDeArray(a, 3);
+		a = BorraDeArray(a, 2);
 		
 		for (int i = 0; i < a.length; i++)
 			System.out.println(a[i]);
