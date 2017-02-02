98a8777e3b9f17317a94773d3f95ffdcf27da7d6
cuent <cuent@DESKTOP-STD0GPV>
2017-02-02 07:09:32 +0100

    Creada estructura base ej36

---
+++ src/Ejercicio36/Ejercicio36.java
@@ -0,0 +1,21 @@
+package Ejercicio36;
+
+public class Ejercicio36 {
+
+	public static void main(String[] args) {
+
+		int[] miArrayOriginal = {0,1,2,3,4,5,2,6,7,8,2,2,9};
+		miArrayOriginal = EliminaElementosArray(miArrayOriginal, 2);
+		
+		for(int i = 0; i < miArrayOriginal.length; i++)
+			System.out.println(miArrayOriginal[i]);
+		
+	}
+	
+	public static int[] EliminaElementosArray(int[] array, int valor){
+		int[] nuevoArray = new int[array.length];
+		
+		
+		return nuevoArray;
+	}
+}
