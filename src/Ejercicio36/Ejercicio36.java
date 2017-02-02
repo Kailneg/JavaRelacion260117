b61297154f740af51cc591297b1f13e9fb5397fe
cuent <cuent@DESKTOP-STD0GPV>
2017-02-02 07:38:05 +0100

    Terminado ej36

--- src/Ejercicio36/Ejercicio36.java
+++ src/Ejercicio36/Ejercicio36.java
@@ -4,18 +4,31 @@ public class Ejercicio36 {
 
 	public static void main(String[] args) {
 
-		int[] miArrayOriginal = {0,1,2,3,4,5,2,6,7,8,2,2,9};
+		int[] miArrayOriginal = { 0, 1, 2, 4, 8, 2, 2, 9 };
 		miArrayOriginal = EliminaElementosArray(miArrayOriginal, 2);
-		
-		for(int i = 0; i < miArrayOriginal.length; i++)
+
+		for (int i = 0; i < miArrayOriginal.length; i++)
 			System.out.println(miArrayOriginal[i]);
-		
+
 	}
-	
-	public static int[] EliminaElementosArray(int[] array, int valor){
-		int[] nuevoArray = new int[array.length];
-		
-		
+
+	public static int[] EliminaElementosArray(int[] array, int valor) {
+		// Calcular tamaño
+		int tamaño = array.length;
+		for (int i = 0; i < array.length; i++)
+			if (array[i] == valor)
+				tamaño--;
+
+		// Poblar array sin los valores
+		int[] nuevoArray = new int[tamaño];
+		int contador = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == valor)
+				continue;
+
+			nuevoArray[contador] = array[i];
+			contador++;
+		}
 		return nuevoArray;
 	}
 }
