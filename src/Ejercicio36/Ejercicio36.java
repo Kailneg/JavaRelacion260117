1ff943cce4a88bb8c2128c60f42f755fd78e7fdc
admincenec <admincenec@PC104.cenecmalaga.net>
2017-02-02 09:13:47 +0100

    Ejericicio 36. Mejorada eficiencia gracias a @pawerino

--- src/Ejercicio36/Ejercicio36.java
+++ src/Ejercicio36/Ejercicio36.java
@@ -4,7 +4,7 @@ public class Ejercicio36 {
 
 	public static void main(String[] args) {
 
-		int[] miArrayOriginal = { 0, 1, 2, 4, 8, 2, 2, 9 };
+		int[] miArrayOriginal = { 0, 1, 2, 3 };
 		miArrayOriginal = EliminaElementosArray(miArrayOriginal, 2);
 
 		for (int i = 0; i < miArrayOriginal.length; i++)
@@ -21,13 +21,12 @@ public class Ejercicio36 {
 
 		// Poblar array sin los valores
 		int[] nuevoArray = new int[tamaño];
-		int contador = 0;
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0, j = 0; i < array.length; i++) {
 			if (array[i] == valor)
 				continue;
 
-			nuevoArray[contador] = array[i];
-			contador++;
+			nuevoArray[j] = array[i];
+			j++;
 		}
 		return nuevoArray;
 	}
